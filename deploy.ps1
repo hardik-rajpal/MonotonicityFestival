14fac15aed30da089e2e8db4763e0e6c3b81d0ba
Hardik <hardikrajpal@cse.iitb.ac.in>
2024-03-06 14:39:07 +0530

    new dev deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,14 +1,12 @@
-if ($args.Length -ne 1) {
-    Write-Host "Usage: powershell deploy.ps1 <baseURL>. Ex: powershell deploy.ps1 https://www.isid.ac.in/~testdeploy/"
-    exit
-}
-
-# Remove directories and files
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue .angular, node_modules/.cache, dist, deploy
-
-# Build the Angular app
-ng build --base-href $args[0]
-
-mkdir deploy
-# Copy files to the deploy directory
-Copy-Item -Path "dist\isid-cecfee\*" -Destination .\deploy -Recurse
+bash -c 'rm -rf .angular node_modules/.cache dist'
+git checkout deploy
+git merge master
+bash -c 'rm main.*.js polyfills.*.js runtime.*.js styles.*.css index.html'    
+ng build --base-href https://hardik-rajpal.github.io/MonotonicityFestival/;
+bash -c 'mv dist/monotonicity-festival/* .';
+bash -c 'cp index.html 404.html; rm -rf assets; cp -r src/assets assets';
+git add -A;
+$msg = bash -c "git log -2 --pretty=%B | sed -n '3 p'"
+git commit -m "$msg";
+git push origin deploy --force;
+git checkout master;
